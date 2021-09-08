c0ad29bf696966a0942c40c8651a0306adcde5fb
= <geraldomartins10@gmail.com>
2021-09-08 01:18:24 -0300

    Criação da classe Conta.kt

---
+++ src/main/kotlin/one/digialinnovation/digibank/Conta.kt
@@ -0,0 +1,18 @@
+package one.digialinnovation.digibank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numeroConta: String,
+    val saldoConta: BigDecimal
+) {
+
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
