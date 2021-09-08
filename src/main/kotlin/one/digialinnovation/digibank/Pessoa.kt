7de3810ad93dbe45d542aa6ce2dae3f3c615d3d5
= <geraldomartins10@gmail.com>
2021-09-08 00:58:17 -0300

    Criação do método contrutor

--- src/main/kotlin/one/digialinnovation/digibank/Pessoa.kt
+++ src/main/kotlin/one/digialinnovation/digibank/Pessoa.kt
@@ -4,30 +4,24 @@ class Pessoa {
 
     var nome: String = "Júnior Moura"
     var cpf: String = "123.456.789-10"
-<<<<<<< HEAD
 
-    inner class Endereco{
-        var rua = "Rua dos Alfeneiros"
-        var numero = "02"
+    private set
 
-    }
-=======
->>>>>>> 1ad0058fabeb2434ccfe98cb0ac89d5ff8221dc6
+    constructor()
+
+    fun pessoaInfo()= "$nome e $cpf"
+
 }
 
 fun main() {
 
     val funcionario = Pessoa()
 
-    println(funcionario.nome)
-    println(funcionario.cpf)
-<<<<<<< HEAD
+    println(funcionario.pessoaInfo())
 
-    println("Rua: " + funcionario.Endereco().rua)
-    println("Numero: " + funcionario.Endereco().numero)
+
+
 
 
 }
-=======
-}
->>>>>>> 1ad0058fabeb2434ccfe98cb0ac89d5ff8221dc6
+
