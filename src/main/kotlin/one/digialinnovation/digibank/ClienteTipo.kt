5b8493f2a28aaba1be4675eafbc45840c1c0792a
= <geraldomartins10@gmail.com>
2021-09-08 23:45:20 -0300

    Criação da classe ClienteTipo.kt do tipo enum

---
+++ src/main/kotlin/one/digialinnovation/digibank/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package one.digialinnovation.digibank
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Física"),
+    PJ("Pessoa Juridica")
+}
---
+++ src/main/kotlin/one/digialinnovation/digibank/Testes/TesteClienteTipoEnum.kt
@@ -0,0 +1,22 @@
+package one.digialinnovation.digibank.Testes
+
+import one.digialinnovation.digibank.ClienteTipo
+
+fun main() {
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+
+    }
+
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+
+    }
+
+    val pf = ClienteTipo.PF
+    println(">>${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">>${pj.name} - ${pj.descricao}")
+}
